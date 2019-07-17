eeac450f35f7c9cdf2b07035beb0839fbaa8d16d
nuthanreddyH <nuthanreddy1194@gmail.com>
2019-07-17 15:36:15 +0530

    overview

--- src/components/CompanyDetails/CompanyDetails.js
+++ src/components/CompanyDetails/CompanyDetails.js
@@ -1,13 +1,48 @@
 import React, { Component } from "react";
+import { companyDetails } from '../../mock.js';
+import {
+  Card,
+  CardHeader,
+  CardFooter, CardBody,
+} from "reactstrap";
 
 
 class CompanyDetails extends Component {
 
+
+  renderCards() {
+    return companyDetails[0].employees.map((item,index) => {
+      return (
+        <Card key={index} className="cardDetails">
+              <CardHeader>{item.personName}</CardHeader>
+              <CardBody>
+                    <label><strong>Address : </strong></label> {item.personAddress} <br/>
+                                  </CardBody>
+              <CardFooter />
+        </Card>
+      )
+})
+  }
+
   
   render() {
-      console.log(this.props.location.state);
     return (
-      <div>Details</div>
+      <div className="ccontainer">
+        <Card>
+            <CardHeader>Profile Overview</CardHeader>
+            <CardBody>
+                      <label><strong>Address : </strong></label> {companyDetails[0].address} <br/>
+                      <label><strong>Revnue : </strong></label> {companyDetails[0].revenue}<br/>
+                      <label><strong>Phone : </strong></label> {companyDetails[0].phone} <br />
+                      <label><strong>No of Employees : </strong></label> {companyDetails[0].employees.length}
+                </CardBody>
+                <CardFooter/>
+        </Card>
+        <Card>
+            <CardHeader>Employees</CardHeader>
+           {this.renderCards()}
+        </Card>
+      </div>
     );
   }
 }
--- src/mock.js
+++ src/mock.js
@@ -3,19 +3,49 @@ export const companyDetails = [
         companyName: 'Amazon',
         address: '410 Terry Ave Seattle,WA',
         revenue: '100000',
+        employees: [
+            {
+                personName: "Robert",
+                PersonAddress: "West"
+            },
+            {
+                personName: "Robert",
+                PersonAddress: "West"
+            },
+            {
+                personName: "Robert",
+                PersonAddress: "West"
+            },
+        ],
         phone: '(022)-266-0000'
     },
     {
         companyName: 'Google',
         address: '1600 Amphitheatre Parkway',
         revenue: '200000',
-        phone: '(022)-246-0000'
+        phone: '(022)-246-0000',
+        employees: [
+            {
+                personName: "Remo",
+                PersonAddress: "East NA"
+            }
+        ],
     },
     {
         companyName: 'Epam',
         address: 'Hyderabad',
         revenue: '240000',
-        phone: '(122)-266-0000'
+        phone: '(122)-266-0000',
+        employees: [
+            {
+                personName: "Robert",
+                PersonAddress: "West"
+            },
+            {
+                personName: "ERIC",
+                PersonAddress: "carolina"
+            },
+        ],
     }
 ]
 
